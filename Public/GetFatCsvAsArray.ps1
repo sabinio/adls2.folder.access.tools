4f659444b0334f4b89d8ea8344ffd783574144cf
Richie Lee <richie@bzzzt.io>
2021-01-11 11:43:36 +0000

    recurse fix

--- Public/GetFatCsvAsArray.ps1
+++ Public/GetFatCsvAsArray.ps1
@@ -9,7 +9,7 @@ Function Get-FatCsvAsArray {
         $myCsv = $myCsv | Where-Object -Property Recurse -eq "True" | Group-Object Container, Folder, Recurse | ForEach-Object { [pscustomobject]@{Container = $_.Name.split(",")[0].Trim(); Folder = $_.Name.split(",")[1].Trim(); Recurse = $_.Name.split(",")[2].Trim(); Items = $_.Group } }
     }
     else {
-        $myCsv = $myCsv | Where-Object -Property Recurse -eq "False" | Group-Object Container, Folder, Recurse | ForEach-Object { [pscustomobject]@{Container = $_.Name.split(",")[0].Trim(); Folder = $_.Name.split(",")[1].Trim(); Recurse = $_.Name.split(",")[2].Trim(); Items = $_.Group } }
+        $myCsv = $myCsv | Where-Object -Property Recurse -in "True", "False" | Group-Object Container, Folder, Recurse | ForEach-Object { [pscustomobject]@{Container = $_.Name.split(",")[0].Trim(); Folder = $_.Name.split(",")[1].Trim(); Recurse = $_.Name.split(",")[2].Trim(); Items = $_.Group } }
     }
     return $myCsv
 }
