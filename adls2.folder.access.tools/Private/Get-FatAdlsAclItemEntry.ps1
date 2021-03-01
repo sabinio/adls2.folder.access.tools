65292c81c27fd05b2dbf80c77cb3c9a2bf36176c
Richie Lee <richie@bzzzt.io>
2021-03-01 13:18:04 +0000

    removing unused functions

--- adls2.folder.access.tools/Private/Get-FatAdlsAclItemEntry.ps1
+++
@@ -1,9 +0,0 @@
-Function Get-FatAdlsAclItemEntry {
-    param(
-        [parameter(Mandatory = $true)] [string]$dataLakeStoreName,
-        [parameter(Mandatory = $true)] [string]$path
-
-    )
-    $aclEntry = Get-AzDataLakeStoreItemAclEntry -AccountName $dataLakeStoreName -Path $path
-    return $aclEntry
-}
