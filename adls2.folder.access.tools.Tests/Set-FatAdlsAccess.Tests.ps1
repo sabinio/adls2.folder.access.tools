16a9a410185475292c6f79efd5c4f382f199d514
Richie Lee <richie@bzzzt.io>
2021-03-08 10:23:54 +0000

    add checks on tests

--- adls2.folder.access.tools.Tests/Set-FatAdlsAccess.Tests.ps1
+++ adls2.folder.access.tools.Tests/Set-FatAdlsAccess.Tests.ps1
@@ -2,7 +2,7 @@ param($ModulePath, $config)
 BeforeAll {
     $CommandName = 'Set-FatAdlsAccess.ps1'
     if (-not $ModulePath) { $ModulePath = join-path (join-path $PSScriptRoot "..") "adls2.folder.access.tools" }
-    if (-not $config) {$config = (Get-Content (join-path $PSScriptRoot '.\config.json') | ConvertFrom-Json)}
+    if (-not $config) { $config = (Get-Content (join-path $PSScriptRoot '.\config.json') | ConvertFrom-Json) }
     Get-Module adls2.folder.access.tools | remove-module -Force
     $CommandNamePath = Resolve-Path (Join-Path $ModulePath /Public/$CommandName)
     Import-Module $CommandNamePath -Force
@@ -22,8 +22,7 @@ BeforeAll {
     New-FatAzDataLakeContainer -ctx $context -ContainerName $config.testContainerName
 }
 
-AfterAll{
-    
+AfterAll {
     $context = Get-FatAzContextForStorageAccount -resourceGroupName $config.resourceGroupName -dataLakeStoreName $config.dataLakeName
     Remove-FatAzDataLakeContainer -ctx $context -ContainerName $config.testContainerName
 }
@@ -32,11 +31,22 @@ AfterAll{
 Describe "Set-FatAdlsAccess" -Tag 'Integration' {
     Context "Add Permission to root folder" {
         It "Will Not Throw" {
+            [PSCustomObject]$FolderAccess0 = @{Default=$false; Type='Group'; Group='adlsRoot'; Perms='rwx'}
+            [PSCustomObject]$FolderAccess1 = @{Default=$true; Type='Group'; Group='adlsRoot'; Perms='r-x'}
+            $context = Get-FatAzContextForStorageAccount -resourceGroupName $config.resourceGroupName -dataLakeStoreName $config.dataLakeName
             $csvPath = Join-Path $PSScriptRoot csvs/setfataccess.csv
             $csv = Get-FatCsvAsArray -csvPath $csvPath
             Set-FatAdlsAccess -subscriptionName $config.subscriptionName -resourceGroupName $config.resourceGroupName -dataLakeStoreName $config.dataLakeName -aclFolders $csv -entryType "acl" -Verbose
+            $folderAccess = Get-FatAclDetailsOnFolder -ctx $context -ContainerName $config.testContainerName -FolderName $config.testFolderName 
+            $zero = @{}
+            $folderAccess[0].psobject.properties | ForEach-Object  { $zero[$_.Name] = $_.Value }
+            $compare = Compare-Object $zero.Values $FolderAccess0.Values 
+            $compare | Should -BeNullOrEmpty
+            $one = @{}
+            $folderAccess[1].psobject.properties | ForEach-Object  { $one[$_.Name] = $_.Value }
+            $compare = Compare-Object $one.Values $FolderAccess1.Values 
+            $compare | Should -BeNullOrEmpty
         }
-
         It "Will Not Throw" {
             Return
         }
--- adls2.folder.access.tools.Tests/helpers/HelperFunctions.ps1
+++ adls2.folder.access.tools.Tests/helpers/HelperFunctions.ps1
@@ -12,7 +12,18 @@ Function New-FatAzDataLakeContainer {
         [parameter(Mandatory = $true)][PSCustomObject]$ctx,
         [parameter(Mandatory = $true)][string]$ContainerName
     )
-    New-AzStorageContainer -Context $ctx -Name $ContainerName 
+    do {
+        Start-Sleep -Seconds 2
+            $container = Get-AzStorageContainer -Context $ctx -Name $ContainerName -ErrorAction SilentlyContinue -ErrorVariable nocontainer | Out-Null
+    }
+    until ($nocontainer)
+    Do {
+        Start-Sleep -Seconds 2
+        $container = $null 
+        New-AzStorageContainer -Context $ctx -Name $ContainerName -ErrorVariable container -ErrorAction SilentlyContinue
+    }
+    until
+    ($container.Exception.Status -ne 409 )
 }
 
 Function Remove-FatAzDataLakeContainer {
@@ -21,10 +32,10 @@ Function Remove-FatAzDataLakeContainer {
         [parameter(Mandatory = $true)][PSCustomObject]$ctx,
         [parameter(Mandatory = $true)][string]$ContainerName
     )
-    Remove-AzStorageContainer -Context $ctx -Name $ContainerName -Force
+    Remove-AzStorageContainer -Context $ctx -Name $ContainerName -Force -ErrorAction SilentlyCOntinue | Out-Null
     do {
         Start-Sleep -Seconds 2
-        $delete = Get-AzStorageContainer -Context $ctx -Name $ContainerName -ErrorAction Continue -ErrorVariable nocontainer
+        $delete = Get-AzStorageContainer -Context $ctx -Name $ContainerName -ErrorAction SilentlyContinue -ErrorVariable nocontainer | Out-Null
     } until ($nocontainer)
 }
 
@@ -38,4 +49,26 @@ Function Get-FatAzContextForStorageAccount {
     $storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -AccountName $dataLakeStoreName
     $ctx = $storageAccount.Context
     return $ctx
+}
+
+Function Get-FatAclDetailsOnFolder {
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory = $true)][PSCustomObject]$ctx,
+        [parameter(Mandatory = $true)][string]$ContainerName,
+        [parameter(Mandatory = $true)][string]$FolderName
+    )
+    $Gen2Item = Get-AzDataLakeGen2Item -Context $ctx -FileSystem $ContainerName -Path $FolderName
+    $aclList = [Collections.Generic.List[System.Object]]($Gen2Item.ACL)
+    
+    Return ($aclList | Where-Object { @("User", "Group") -contains $_.AccessControlType -and $null -ne $_.EntityId } | `
+            ForEach-Object { 
+            $ADGroup = Get-FatCachedAdGroupName -ObjectId $_.EntityId
+            $ADGroupDisplayName = $ADGroup.DisplayName
+            [PSCustomObject]@{Default = $_.DefaultScope;
+                Type                  = "$($_.AccessControlType)";
+                Group                 = $ADGroupDisplayName; 
+                Perms                 = $_.GetSymbolicRolePermissions()
+            } 
+        } )
 }
